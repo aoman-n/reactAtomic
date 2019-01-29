115e0e0b5fb7c665a0be4a4a944ca5246fbead45
laster18 <ipbdipbd00@gmail.com>
2019-01-29 14:19:15 +0900

    Update button styles

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 import { colors } from '../../../styles';
 
-const Container = styled.button`
+const getColor = props => {
+  if (props.primary) {
+    return 'red';
+  }
+  if (props.secondary) {
+    return 'blue';
+  }
+  return `${colors.yellow}`;
+};
+
+const styles = css`
+  display: inline-flex;
+  align-items: center;
+  white-space: nowrap;
+  justify-content: center;
+  text-decoration: none;
+  appearance: none;
+  box-sizing: border-box;
   height: 40px;
   width: 216px;
-  box-shadow: 0px 0px 0px 3px
-    ${props => (props.disabled ? colors.lightGray : colors.yellow)};
   border: solid 2px rgba(255, 255, 255, 0.47);
-  background: ${props => (props.disabled ? colors.lightGray : colors.yellow)};
-  color: ${props => (props.disabled ? '#DFDFDF' : colors.white)};
+  ${props =>
+    props.disabled
+      ? css`
+          box-shadow: 0px 0px 0px 3px ${colors.lightGray};
+          background: ${colors.lightGray};
+          color: #dfdfdf;
+        `
+      : css`
+          box-shadow: 0px 0px 0px 3px ${getColor(props)};
+          background: ${getColor(props)};
+          color: ${colors.white};
+        `}
   font-size: 16px;
   font-weight: bold;
   cursor: pointer;
@@ -19,8 +45,21 @@ const Container = styled.button`
   }
 `;
 
+const StyledButton = styled.button`
+  ${styles}
+`;
+
+const LinkButton = styled(({ children, ...props }) => (
+  <Link {...props}>{children}</Link>
+))`
+  ${styles}
+`;
+
 const Button = ({ children, ...props }) => {
-  return <Container {...props}>{children}</Container>;
+  if (props.to) {
+    return <LinkButton {...props}>{children}</LinkButton>;
+  }
+  return <StyledButton {...props}>{children}</StyledButton>;
 };
 
 export default Button;
--- src/components/atoms/Button/index.stories.js
+++ src/components/atoms/Button/index.stories.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import Button from '.';
 
 storiesOf('Button', module)
   .addDecorator(withKnobs)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={['/', 'items']}>{story()}</MemoryRouter>
+  ))
   .add('default', () => (
     <Button disabled={boolean('Disabled', false)}>
       {text('Label', '予約はこちら')}
     </Button>
-  ));
+  ))
+  .add('primary', () => (
+    <Button primary disabled={boolean('Disabled', false)}>
+      {text('Label', '予約はこちら')}
+    </Button>
+  ))
+  .add('link', () => <Button to="/items">itemsへ</Button>);
